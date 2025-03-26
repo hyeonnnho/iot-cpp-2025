58fdc5ce276266ab9df6f071744e6a07e87b6bf5
hyeonnnho <tiger1330@naver.com>
2025-03-26 16:49:12 +0900

    DB 커밋

--- DB/DB.cpp
+++ DB/DB.cpp
@@ -1,25 +1,28 @@
-#include <iostream>
-#include <mysql/jdbc.h>
-
-using namespace std;
-using namespace sql;
-
-int main()
-{
-	try {
-		mysql::MySQL_Driver* driver;
-		Connection* con;
-
-		driver = mysql::get_mysql_driver_instance();
-
-		con = driver->connect("tcp://127.0.0.1:3306", "madang", "12345");
-		con->setSchema("madang");
-
-		cout << "Connection successful!!" << endl;
-	}
-	catch (SQLException& e) {
-		cerr << "MySQL Connection failed!!" << e.what() << endl;
-	}
-
-	return 0;
-}
+///*
+//	DB ¿¬°á
+//*/
+//#include <iostream>
+//#include <mysql/jdbc.h>
+//
+//using namespace std;
+//using namespace sql;
+//
+//int main()
+//{
+//	try {
+//		mysql::MySQL_Driver* driver;
+//		Connection* con;
+//
+//		driver = mysql::get_mysql_driver_instance();
+//
+//		con = driver->connect("tcp://127.0.0.1:3306", "madang", "12345");
+//		con->setSchema("madang");
+//
+//		cout << "Connection successful!!" << endl;
+//	}
+//	catch (SQLException& e) {
+//		cerr << "MySQL Connection failed!!" << e.what() << endl;
+//	}
+//
+//	return 0;
+//}
